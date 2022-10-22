feb3c5f546cb40f7d9159fef173a167e2c501eae
Paraizo <felipeparaizo@gmail.com>
2022-10-22 16:34:38 -0300

    fix in entrypoint

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	planet "github.com/paraizofelipe/star-planet/planet/handler"
 	"github.com/paraizofelipe/star-planet/settings"
 )
 
@@ -16,12 +17,17 @@ func main() {
 
 	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
 
-	_, err = sqlx.Open("postgres", settings.Storage)
+	storage, err := sqlx.Open("postgres", settings.Storage)
 	if err != nil {
 		log.Panic(err)
 	}
 
+	planetHandler := planet.NewHandler(storage, logger)
+	http.HandleFunc("/api/planets/", planetHandler.Router)
+
 	url := fmt.Sprintf("%s:%d", settings.Host, settings.Port)
+
+	log.Printf("Server listening in %s", url)
 
 	if err = http.ListenAndServe(url, nil); err != nil {
 		logger.Fatal(err)
