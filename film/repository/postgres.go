05c9cd59e50d8f7a194c3a1804697214abb00e1a
Paraizo <felipeparaizo@gmail.com>
2022-10-24 02:37:06 -0300

    insert list of films in planet response

--- film/repository/postgres.go
+++ film/repository/postgres.go
@@ -51,48 +51,6 @@ func (r repository) Add(film domain.Film) (err error) {
 	return
 }
 
-func (r repository) UpdateOrAdd(film domain.Film) (err error) {
-	statement := `
-        INSERT INTO films (
-            id,
-            planet_id,
-            title,
-            director,
-            Release_date,
-            created_at,
-            updated_at
-        ) VALUES (
-            $1,
-            $2,
-            $3,
-            $4,
-            $5,
-            $6,
-            $7
-        ) ON CONFLICT (id)
-        DO UPDATE SET 
-            id = $1,
-            planet_id = $2
-            title = $3,
-            director = $4,
-            Release_date = $5,
-            created_at = $6,
-            updated_at = $7
-        WHERE
-            films.id = $1;
-    `
-	err = r.storage.Exec(statement,
-		film.ID,
-		film.PlanetID,
-		film.Title,
-		film.Director,
-		film.ReleaseDate,
-		time.Now().UTC(),
-		time.Now().UTC(),
-	)
-	return
-}
-
 func (r repository) FindByID(id int) (film domain.Film, err error) {
 	statement := `
         SELECT
